10a8a262fa93039f3dcbb9cac40d8bf93418d742
Jack Sanders <personal@jacksanders.uk>
2025-07-22 17:48:56 +0100

    feat: added git aliases

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -10,7 +10,28 @@
     cd = "z";
     ll = "eza -l -a --icons --group-directories-first";
 
-    # Git aliases
+    # Git aliases from omz
+    g = "git";
+    ga = "git add";
+    gb = "git branch";
+    gc = "git commit -v";
+    gco = "git checkout";
+    gcl = "git clone --recurse-submodules";
+    gcp = "git cherry-pick";
+    gd = "git diff";
+    gf = "git fetch";
+    glg = "git log --stat";
+    gm = "git merge";
+    gr = "git remote";
+    gra = "git remote add";
+    grb = "git rebase";
+    grh = "git reset";
+    grhh = "git reset --hard";
+    grm = "git rm";
+    grst = "git restore --staged";
+    gst = "git status";
+
+    # Custom git aliases
     gpsh = "git push origin";
     gpll = "git pull origin";
   };
