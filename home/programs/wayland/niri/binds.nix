ebcf8bfd8f0eb1b038aeec874af2142c10b67975
Jack Sanders <personal@jacksanders.uk>
2025-08-04 15:54:09 +0100

    feat(keybinds): added more useful keybinds

--- home/programs/wayland/niri/binds.nix
+++ home/programs/wayland/niri/binds.nix
@@ -27,38 +27,44 @@
     "Print".action.screenshot-screen = {write-to-disk = true;};
     "Mod+Shift+Alt+S".action = screenshot-window;
     "Mod+Shift+S".action.screenshot = {show-pointer = false;};
-    "Mod+Shift+C".action = spawn "${pkgs.eyedropper}/bin/eyedropper";
+    "Mod+Shift+P".action = spawn "${pkgs.eyedropper}/bin/eyedropper";
    
     # ==== Spawn Programs ==== # 
-    "Mod+Space".action = spawn "${pkgs.anyrun}/bin/anyrun";
     "Mod+Return".action = spawn "${pkgs.kitty}/bin/kitty";
     "Mod+F".action = spawn "${pkgs.nautilus}/bin/nautilus";
     "Mod+W".action = spawn "${pkgs.firefox-devedition}/bin/firefox-devedition";
 
     "Mod+U".action = spawn "env XDG_CURRENT_DESKTOP=gnome gnome-control-center";
+    
+    # ==== Overview/Launcher/Finder ==== #
+    "Mod+Tab".action = toggle-overview;
+    "Mod+Space".action = spawn "${pkgs.anyrun}/bin/anyrun";
 
-    "Mod+Q".action = close-window;
-    "Mod+S".action = switch-preset-column-width;
-    "Mod+M".action = maximize-column;
-
+    # ==== Adjust Windows ==== #
     "Mod+1".action = set-column-width "25%";
     "Mod+2".action = set-column-width "50%";
     "Mod+3".action = set-column-width "75%";
     "Mod+4".action = set-column-width "100%";
+    "Mod+Minus".action = set-column-width "-10%";
+    "Mod+Plus".action = set-column-width "+10%";
+    "Mod+Shift+Minus".action = set-window-height "-10%";
+    "Mod+Shift+Plus".action = set-window-height "+10%";
+    
+    "Mod+V".action = toggle-window-floating;
+    "Mod+C".action = center-window;
+    "Mod+Shift+C".action = center-visible-columns;
+
+    "Mod+Q".action = close-window;
+    "Mod+S".action = switch-preset-column-width;
+    "Mod+M".action = maximize-column;
+    "Mod+Shift+M".action = fullscreen-window;
     # "Mod+Shift+F".action = fullscreen-window;
     "Mod+Shift+F".action = expand-column-to-available-width;
 
     "Mod+Comma".action = consume-window-into-column;
     "Mod+Period".action = expel-window-from-column;
-    "Mod+C".action = center-visible-columns;
-    "Mod+Tab".action = switch-focus-between-floating-and-tiling;
 
-    "Mod+Minus".action = set-column-width "-10%";
-    "Mod+Plus".action = set-column-width "+10%";
-    "Mod+Shift+Minus".action = set-window-height "-10%";
-    "Mod+Shift+Plus".action = set-window-height "+10%";
-
-    # ==== Workspace/Column Keybinds ==== #
+    # ==== Change Focus ==== #
     "Mod+WheelScrollLeft".action = focus-column-left;
     "Mod+WheelScrollRight".action = focus-column-right;
     "Mod+Shift+WheelScrollUp".action = focus-column-left;
