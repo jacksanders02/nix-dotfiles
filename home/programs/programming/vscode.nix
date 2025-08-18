4b5de487c1743521ae21315036a79ab6c9fbcef2
Jack Sanders <personal@jacksanders.uk>
2025-08-18 17:45:07 +0100

    feat(vs-code): added VS Code

--- home/programs/programming/vscode.nix
+++ home/programs/programming/vscode.nix
@@ -5,4 +5,20 @@
     enable = true;
     package = pkgs.vscode.fhs;
   };
+  
+  # Fix OS keyring detection
+  # https://errorism.dev/issues/microsoft-vscode-linux-an-os-keyring-couldnt-be-identified-for-storing-the-encryption-related-data-in-your-current-
+  home.file.".vscode/argv.json" = {
+    force = true;
+    text = ''
+      {
+      	// "disable-hardware-acceleration": true,
+      	"enable-crash-reporter": true,
+      	// Unique id used for correlating crash reports sent from this instance.
+      	// Do not edit this value.
+      	"crash-reporter-id": "YOUR_CRASH_ID_HERE",
+      	"password-store": "gnome"
+      }
+    '';
+  };
 }
